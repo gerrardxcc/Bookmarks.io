46e3d9c1c3c838ac21b0ba546110d0168f35b66a
GerrardXiong <chengcheng.xiong@my.jcu.edu.au>
2022-02-24 11:28:18 +1000

    Import agron to allow users to generate the hash password

--- src/auth /auth.controller.ts
+++ src/auth /auth.controller.ts
@@ -7,7 +7,7 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signup(@Body() dto: AuthDto) {  
+  signup(@Body() dto: AuthDto) {
     return this.authService.signup(dto);
   }
 
--- src/auth /auth.service.ts
+++ src/auth /auth.service.ts
@@ -1,12 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
+import * as argon from 'argon2';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  signup(dto: AuthDto) {
-    return { msg: 'I have signed up' };
+
+  async signup(dto: AuthDto) {
+    // generate password hash
+    const hash = await argon.hash(dto.password);
+    //save the new user in the db
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash,
+      },
+    });
+    //return the saved user
+    return user;
   }
   signin() {
     return { msg: 'I have signed in' };
