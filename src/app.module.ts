990b92369168afb6cd10ee7e9c636907bfc056c8
GerrardXiong <chengcheng.xiong@my.jcu.edu.au>
2022-02-25 02:13:56 +1000

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,6 @@ import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaModule } from './prisma/prisma.module';
 
-
 @Module({
   imports: [
     ConfigModule.forRoot({
