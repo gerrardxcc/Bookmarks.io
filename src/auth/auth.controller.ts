64fe6e423d94faabd50f632fc6417d96676f3a91
GerrardXiong <chengcheng.xiong@my.jcu.edu.au>
2022-02-26 01:21:09 +1000

    updates

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,10 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
 
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,9 @@
-import { Controller, Get, Patch, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Patch,
+  UseGuards,
+} from '@nestjs/common';
 import { User } from '@prisma/client';
 import { GetUser } from 'src/auth/decorator';
 import { JwtGuard } from 'src/auth/guard';
